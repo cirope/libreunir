dcf8ba74f867c9f97aed4b79a8bfb3afa78461f1
Franco Catena <francocatena@gmail.com>
2013-04-08 18:20:26 -0300

    Config deploy files added

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+require 'bundler/capistrano'
+set :bundle_flags, '--deployment --quiet --binstubs'
+set (:bundle_cmd) { "#{release_path}/bin/bundle" }
+
+server 'libreunir.com', :web, :app, :db, primary: true
+
+set :application, 'libreunir'
+set :user, 'deployer'
+set :deploy_to, '/var/rails/#{application}'
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, :git
+set :repository,  'git://github.com/kainlite/libreunir.git'
+set :branch, 'master'
+
+after 'deploy:restart', 'deploy:cleanup'
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: { no_release: true } do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read('config/app_config.example.yml'), "#{shared_path}/config/app_config.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after 'deploy:setup', 'deploy:setup_config'
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/app_config.yml #{release_path}/config/app_config.yml"
+  end
+  after 'deploy:finalize_update', 'deploy:symlink_config'
+
+  desc 'Make sure local git is in sync with remote.'
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts 'WARNING: HEAD is not the same as origin/master'
+      puts 'Run `git push` to sync changes.'
+      exit
+    end
+  end
+  before 'deploy', 'deploy:check_revision'
+end
