5d50eac35852e29d48d9448b6054896e3bb74300
Franco Catena <francocatena@gmail.com>
2013-04-24 23:04:58 -0300

    Mini refactor in formatter

--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,31 +1,31 @@
+require 'fileutils'
+
 class Formatter
-  require 'fileutils'
+  PROCESSED_PATH = 'private/data/processed'
 
   def initialize
     @current_date = Time.new.strftime('%Y-%m-%d')
-    @formatter = Logger.new("#{Rails.root}/log/formatter-#{@current_date}.log")
-    @processed = "private/data/processed"
+    @logger = create_logger
   end
 
   def info(klass, row, attributes)
-    message = String.new
-    message << "#{klass} - Row: #{row}"
+    message  = "#{klass} - Row: #{row}"
     message << "#{klass} - Record not saved : #{attributes}"
 
-    @formatter.info(message)
+    @logger.info(message)
   end
 
   def create_directories
-    FileUtils.mkdir_p(File.expand_path("private/backup"))
-    FileUtils.mkdir_p(File.expand_path(@processed))
-    FileUtils.mkdir_p(File.expand_path(@processed) + '/' + @current_date)
+    FileUtils.mkdir_p(File.expand_path('private/backup'))
+    FileUtils.mkdir_p(File.expand_path(PROCESSED_PATH))
+    FileUtils.mkdir_p(File.expand_path(PROCESSED_PATH) + '/' + @current_date)
   end
 
   def backup_zip_file
     filename = APP_CONFIG['zip']['filename']
 
-    origin = File.expand_path("private") + '/' + filename
-    target = File.expand_path("private/backup/") + '/' + "#{filename.gsub('.zip', '')}-#{@current_date}.zip"
+    origin = File.expand_path('private') + '/' + filename
+    target = File.expand_path('private/backup/') + '/' + "#{filename.gsub('.zip', '')}-#{@current_date}.zip"
 
     FileUtils.mv(origin, target) if File.exists?(origin)
   end
@@ -33,11 +33,19 @@ class Formatter
   def move_processed(path)
     FileUtils.mv(
       path,
-      File.expand_path(@processed) + '/' + "#{@current_date}/#{File.basename(path).downcase}"
+      "#{File.expand_path(PROCESSED_PATH)}/#{@current_date}/#{File.basename(path).downcase}"
     )
   end
 
   def cleanup(klass)
     klass.where('updated_at < :today', today: Time.now.midnight).find_each(&:destroy)
+  end
+
+  def create_logger
+    log_path = "#{Rails.root}/log/parser.log"
+
+    FileUtils.touch log_path unless File.exists?(log_path)
+
+    Logger.new(log_path)
   end
 end
