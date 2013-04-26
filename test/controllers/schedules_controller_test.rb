308bd3346d5fff5b0587b7260c91db05e7bf6c70
Franco Catena <francocatena@gmail.com>
2013-04-26 10:13:31 -0300

    Schedule destroy imposible =)

--- test/controllers/schedules_controller_test.rb
+++ test/controllers/schedules_controller_test.rb
@@ -47,7 +47,8 @@ class SchedulesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:schedule)
     assert_select '#unexpected_error', false
-    assert_template 'schedules/show', format: :js
+    assert_template 'schedules/show'
+    assert_equal :js, @request.format.symbol
   end
 
   test 'should get edit' do
@@ -63,20 +64,13 @@ class SchedulesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:schedule)
     assert_select '#unexpected_error', false
-    assert_template 'schedules/edit', format: :js
+    assert_template 'schedules/edit'
+    assert_equal :js, @request.format.symbol
   end
 
   test 'should update schedule' do
     put :update, id: @schedule, 
       schedule: Fabricate.attributes_for(:schedule, attr: 'value')
     assert_redirected_to schedule_url(assigns(:schedule))
-  end
-
-  test 'should destroy schedule' do
-    assert_difference('Schedule.count', -1) do
-      delete :destroy, id: @schedule
-    end
-
-    assert_redirected_to schedules_path
   end
 end
