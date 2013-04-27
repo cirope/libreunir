b60ccae105f4418852838dd2cb39baf817564298
Ismael Isuani <ismaelisuani@gmail.com>
2013-04-27 11:40:25 -0300

    Fix js templates

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -28,4 +28,14 @@ module SchedulesHelper
 
     schedule.done ? label : link_to(label, schedule,  data: { remote: true })
   end
+
+  def link_to_cancel_schedule(schedulable)
+    if schedulable
+      data = { remove_target: "[data-schedulable-id=\"#{schedulable.to_param}\"]" }
+    else
+      data = { empty_target: '[data-schedule-place-holder]' }
+    end
+
+    link_to(t('label.cancel'), '#', class: 'btn btn-mini', data: data)
+  end
 end
