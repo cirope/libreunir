d3644ff409f6704d13d9154e11a274992f52e7fc
Franco Catena <francocatena@gmail.com>
2013-05-13 14:45:54 -0300

    Correct namespace in sidekiq initializer...

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/10', namespace: 'x0y0' }
+  config.redis = { url: 'redis://localhost:6379/10', namespace: 'libreunir' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/10', namespace: 'x0y0' }
+  config.redis = { url: 'redis://localhost:6379/10', namespace: 'libreunir' }
 end
