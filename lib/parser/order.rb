553bab2cd20f1d9831817bd4e0039e1c2da02e59
Ismael Isuani <ismaelisuani@gmail.com>
2013-07-16 20:02:50 -0300

    Fix #18

--- lib/parser/order.rb
+++ lib/parser/order.rb
@@ -2,7 +2,7 @@ module Parser
   class Order < Base
     def process_row(row)
       loan = ::Loan.find_by(loan_id: row[0].to_i)
-      user = ::User.find_by(username: row[2])
+      user = ::User.find_by(username: row[45])
       branch = ::Branch.find_by(branch_id: row[7].to_i)
       zone = ::Zone.find_by(zone_id: row[48])
 
