14eb6e1fdfa07b7a67740b28201c8d304408a2f9
Ismael Isuani <ismaelisuani@gmail.com>
2013-08-22 17:04:55 -0300

    order parser changed

--- lib/parser/order.rb
+++ lib/parser/order.rb
@@ -2,10 +2,10 @@ module Parser
   class Order < Base
     def process_row(row)
       loan = ::Loan.find_by(loan_id: row[0].to_i)
-      segment = ::Segment.find_by(segment_id: row[4])
-      branch = ::Branch.find_by(branch_id: row[7].to_i)
-      user = ::User.find_by(username: row[45])
-      zone = ::Zone.find_by(zone_id: row[48])
+      segment = ::Segment.find_by(segment_id: row[2])
+      branch = ::Branch.find_by(branch_id: row[3].to_i)
+      user = ::User.find_by(username: row[5])
+      zone = ::Zone.find_by(zone_id: row[1])
 
       attributes = {
         loan_id: row[0].to_i,
@@ -13,7 +13,7 @@ module Parser
         branch_id: branch.try(:id),
         user_id: user.try(:id), 
         zone_id: zone.try(:id),
-        created_at: row[8]
+        created_at: row[4]
       }
 
       save_instance(loan, ::Loan, attributes)
