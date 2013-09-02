26a91c5c047568d39060498194f51b9f2efac1cf
Ismael Isuani <ismaelisuani@gmail.com>
2013-09-02 10:45:40 -0300

    Improved tanancy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,15 @@
 class ApplicationController < ActionController::Base
   include Application::CancanStrongParameters
+  include Application::Tenancy
 
   protect_from_forgery
 
+  helper_method :selected_user
   before_action :clear_referer
   after_action -> { expires_now if user_signed_in? }
 
   def user_for_paper_trail
     current_user.try(:id)
-  end
-
-  def current_ability
-    @_current_ability ||= Ability.new(selected_user)
   end
 
   rescue_from Exception do |exception|
@@ -28,26 +26,8 @@ class ApplicationController < ActionController::Base
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
+    reset_session
     new_user_session_path
-  end
-
-  def selected_user
-    @_selected_user ||= (current_tenant || current_user)
-  end
-  helper_method :selected_user
-
-  def tenant
-    @_tenant ||= User.find_by(id: session[:tenant_id]) if session[:tenant_id]
-  end
-
-  def current_tenant
-    if tenant
-      unless current_user.can_show?(tenant)
-        redirect_to root_url, alert: t('errors.access_denied')
-      else
-        tenant
-      end
-    end
   end
 
   def today_schedules_count
