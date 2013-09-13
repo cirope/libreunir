f87f5c28b68189e091f8aa4a96ce034686aaea71
Ismael Isuani <ismaelisuani@gmail.com>
2013-09-13 16:03:30 -0300

    Scopes cleanup

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -32,6 +32,6 @@
   end
 
   def self.filter_by_loans(loans)
-    joins(:loans).where("#{Loan.table_name}.id" => loans.ids).uniq
+    joins(:loans).where("#{Loan.table_name}.id" => loans.ids)
   end
 end
