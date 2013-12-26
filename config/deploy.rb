c3c00f0be4d5472d641be032762b2dc3d3fe5b48
Franco Catena <francocatena@gmail.com>
2013-12-26 20:35:59 -0300

    Ruby 2.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
-set :application, 'libreunir'
+set :application, 'libreunir.com'
 set :user, 'deployer'
 set :repo_url, 'git://github.com/cirope/libreunir.git'
 
 set :format, :pretty
 set :log_level, :info
 
-set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to, "/var/www/#{fetch(:application)}"
 set :deploy_via, :remote_cache
 set :scm, :git
 
@@ -13,7 +13,7 @@ set :linked_files, %w{config/app_config.yml}
 set :linked_dirs, %w{log}
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.0.0-p353'
+set :rbenv_ruby, '2.1.0'
 
 set :keep_releases, 5
 
